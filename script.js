88a42b723841c199874d4529332113e0b01654c3
KamilciaHeheh <katuwa@int.pl>
2024-05-12 00:43:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loaderContainer = document.querySelector('.loader-container');
-    },
+    const content = document.querySelector('.content');
+
+    // Pokazujemy loader na początku
+    loaderContainer.style.display = 'block';
+
+    // Po 5 sekundach ukrywamy loader i pokazujemy resztę treści strony
+    setTimeout(function() {
+        loaderContainer.style.display = 'none';
+        content.style.display = 'block';
+    }, 5000);
 });
