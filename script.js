14cbe7a0a649ada4ae6bafe3035814c8bb67523f
KamilciaHeheh <katuwa@int.pl>
2024-06-13 23:19:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     buttons.forEach(button => {
         button.addEventListener('click', (event) => {
             const product = event.target.closest('.product').dataset.product;
-            sendToDiscord(product);
+            sendToServer(product);
         });
     });
 
-    function sendToDiscord(product) {
-        fetch('https://discord.com/api/webhooks/1250920957649092608/b2Y2GprJPfTx7Iah2z8ttuubB7KS-1o-pc2RGQq6OVxkK48bdRhMU1TPIQuK2DEhYlLT', {
+    function sendToServer(product) {
+        fetch('/send-to-discord', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                content: `Klient wybrał: ${product}`
-            })
+            body: JSON.stringify({ product })
         })
-        .then(response => {
-            if (response.ok) {
-                alert('Powiadomienie wysłane!');
-            } else {
-                alert('Błąd przy wysyłaniu powiadomienia.');
-            }
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message);
         })
         .catch(error => {
             console.error('Błąd:', error);
