f6781a967813952e500f463609fd04d7a06e8521
KamilciaHeheh <katuwa@int.pl>
2024-06-13 23:22:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.product button');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const product = event.target.closest('.product').dataset.product;
-            sendToServer(product);
-        });
-    });
-
-    function sendToServer(product) {
-        fetch('/send-to-discord', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ product })
-        })
+function selectProduct(product) {
+    const timestamp = new Date().toISOString();
+    const orderId = `#${Math.floor(Math.random() * 9000000) + 1000000}`;
+    
+    fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
         .then(data => {
-            alert(data.message);
-        })
-        .catch(error => {
-            console.error('Błąd:', error);
+            const ip = data.ip;
+            sendToDiscord(product, timestamp, orderId, ip);
         });
-    }
-});
+}
+
+function sendToDiscord(product, timestamp, orderId, ip) {
+    fetch('/send-to-discord', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            product,
+            timestamp,
+            orderId,
+            ip
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => {
+        console.error('Błąd:', error);
+    });
+}
