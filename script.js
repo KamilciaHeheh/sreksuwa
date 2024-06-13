967198fcc2112ef0e24164fc138eda3f096f73ea
KamilciaHeheh <katuwa@int.pl>
2024-06-13 23:35:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
+const correctPassword = 'paramx442';
+
 function verifyPassword() {
     const password = document.getElementById('password').value;
 
-    fetch('/verify-password', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ password })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            document.getElementById('content').classList.add('active');
-            document.querySelector('.login-container').style.display = 'none';
-        } else {
-            document.getElementById('error-message').style.display = 'block';
-        }
-    })
-    .catch(error => {
-        console.error('Błąd:', error);
-    });
+    if (password === correctPassword) {
+        document.getElementById('content').classList.add('active');
+        document.querySelector('.login-container').style.display = 'none';
+    } else {
+        document.getElementById('error-message').style.display = 'block';
+    }
 }
