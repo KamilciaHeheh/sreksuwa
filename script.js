c64db2595704c205319b5cc513bb1a6ef6cc18e5
KamilciaHeheh <katuwa@int.pl>
2024-06-15 01:04:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function handleClick(element) {
     if (img.style.backgroundColor === 'green') {
         img.style.backgroundColor = 'red';
         status.textContent = 'A231 AWARIA';
+        status.style.color = 'red';
     } else {
         img.style.backgroundColor = 'green';
         status.textContent = 'Gokart';
+        status.style.color = 'green';
     }
 }
